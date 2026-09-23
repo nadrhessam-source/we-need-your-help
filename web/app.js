fe9d7b2fefa77aa7549fe618f70fb62ad6fee8f3
hessam nadr <hessam.nadr.dexter@gmail.com>
2026-09-23 18:12:44 +0330

    Fix connection dot not turning green on success

--- web/app.js
+++ web/app.js
@@ -72,9 +72,8 @@
     }
 
     function hideConnectionBanner() {
-        if (!connectionState.failing) return;
         connectionState.failing = false;
-        var dot = $("#connection-dot");
+        var dot = 20 20 12 61 79 80 81 98 701 33 100 204 250 395 398 399"#connection-dot");
         if (dot) {
             dot.classList.remove("disconnected", "checking");
             dot.classList.add("connected");
